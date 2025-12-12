54e16f244f2f73ba4beb4890cdc23b0423f00092
Alex-Oduor <alexoduor183@gmail.com>
2025-12-12 13:46:35 +0300

    updated main entry oint

--- public/index.php
+++ public/index.php
@@ -1,34 +1,33 @@
 <?php
 
-// Fix missing server vars when running from CLI (eliminates the "Undefined array key" warning)
-$_SERVER['REQUEST_URI']      ??= '/';
-$_SERVER['REQUEST_METHOD']   ??= 'GET';
-$_SERVER['SCRIPT_NAME']      ??= '/index.php';
+// entry point for the application
 
-// Load the app
-require_once __DIR__ . '/../src/controllers/UserController.php';
+require_once __DIR__ .'/../core/Application.php';
+require_once __DIR__ . '/../User/src/controllers/UserController.php';
 
-// Simple router
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); // strips query string
 
-$controller = new UserController();
+$app = new Application();
 
-// GET all users
-if ($uri === '/users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+
+$app->router->get('/users' ,function() {
+    $controller = new UserController();
     header('Content-Type: application/json');
     echo json_encode($controller->getAll());
     exit;
-}
+});
 
-// POST /users/create  add a user
-if ($uri === '/users/create' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+$app->router->post('/users/create' , function() {
+     $controller = new UserController();
     $data = json_decode(file_get_contents('php://input'), true) ?? [];
     $result = $controller->create($data);
 
     header('Content-Type: application/json');
     echo json_encode($result); // return JSON on success/failure
     exit;
-}
+    
+});
 
-// Default welcome message
-echo "AccountLink API is running\n";
+$app->run();
+S
+
+?>
